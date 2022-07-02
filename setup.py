7946c197cc193648a21580748888d2459bb6cf9b
JPhill <joshuataylorphillips@gmail.com>
2022-07-02 17:03:12 -0400

    Setup file edits

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ Created on Sat Jul  2 14:19:28 2022
 
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name='crypto_toolbox',
@@ -16,9 +14,7 @@ setuptools.setup(
     author='Josh Phillips',
     author_email='joshuataylorphillips@gmail.com',
     description='Package for crypto metrics and borrowing/lending analysis',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/JPhill33/crypto_toolbox.git',
+    url='https://github.com/JPhill33/crypto_toolbox',
     license='MIT',
     packages=['crypto_toolbox'],
     install_requires=['requests', 'json', 'urllib.request', 'pandas', 'statistics', 'numpy', 'datetime'],
